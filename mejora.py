d3bb04a5854a1eff4ff36ddee1617ce2b4b6d055
Felipe Canales <f.canales.27@gmail.com>
2016-07-04 22:31:19 -0400

    Update

--- mejora.py
+++ mejora.py
@@ -1,4 +1,6 @@
+import cv2
 import web, sys, os, base64
+p = 4
 
 include_dirs = ['paquetes']
 
@@ -10,17 +12,17 @@ from layout import Layout_main
 urls = (
     '', 'helper',
     '/', 'Index',
-#  Urls ajax para el proceso de imagen
-    '/mejora-brillo', 'MejoraBrillo',
-    '/mejora-contraste', 'MejoraContraste',
+    #  Urls ajax para el proceso de imagen
+    '/mejora-brillo-contraste', 'MejoraBrilloContraste',
     '/mejora-hdr', 'MejoraHDR'
 )
 
 app_mejora = web.application(urls, locals())
 
 static_dir = os.path.abspath(os.path.dirname(__file__)) + '/static'
+algoritmos_path = os.path.abspath(os.path.dirname(__file__)) + '/algoritmos/mejora'
 template_dir = os.path.abspath(os.path.dirname(__file__)) + '/template/mejora'
-img_path = static_dir + '/img/'
+img_path = os.getcwd() + '/images/'
 htmlout = web.template.render(template_dir, base='layout')
 render_plain = web.template.render('template/mejora')
 global message
@@ -40,6 +42,42 @@ app_mejora.add_processor(web.loadhook(variables_locales))
 class helper:
     def GET(self):
         raise web.seeother('/')
+
+
+class MejoraBrilloContraste:
+    def GET(self):
+
+        brillo = (float(web.input().brillo) + 100) / 100
+        contraste = (float(web.input().contraste) + 100) / 100
+
+        print brillo, contraste
+
+        os.system('mpiexec -np %s python %s/bc.py %s %s' % (p, algoritmos_path, contraste, brillo))
+
+        img = cv2.imread(img_path + 'regionEditada_0.jpg', cv2.IMREAD_COLOR)
+
+        _, data = cv2.imencode('.jpg', img)
+        jpg_data = base64.b64decode(data.tostring())
+
+        return jpg_data
+
+class MejoraHDR:
+    def POST(self):
+        x = web.input(myfile={})
+        filedir = os.getcwd() + '/images'  # change this to the directory you want to store the file in.
+        if 'myfile' in x:  # to check if the file-object is created
+            filepath = x.myfile.filename.replace('\\', '/')  # replaces the windows-style slashes with linux ones.
+            filename = filepath.split('/')[-1]  # splits the and chooses the last part (the filename with extension)
+            if filename.split('.')[-1] == 'zip':
+                fout = open(filedir + '/' + filename, 'w')  # creates the file where the uploaded file should be stored
+                fout.write(x.myfile.file.read())  # writes the uploaded file to the newly created file.
+                fout.close()  # closes the file, upload complete.
+                raise web.seeother('/')
+            else:
+                global message
+                message = 'Error, no se puede trabajar con el formato <strong> %s </strong>' % filename.split('.')[-1]
+                raise web.seeother('/')
+
 
 class Index:
     def GET(self):
--- static/js/base2.js
+++ static/js/base2.js
@@ -2,25 +2,37 @@
  * Created by fcanales on 04-07-16.
  */
 
+function setBC() {
+    jQuery('#brightInp').val(sliders2[0].noUiSlider.get());
+    jQuery('#contrastInp').val(sliders2[1].noUiSlider.get());
 
-var brillo = document.getElementById('brillo');
-noUiSlider.create(brillo, {
-    start: 0,
-    range: {'min': -100, 'max': 100},
-    step: 1
+}
+var sliders2 = document.getElementsByClassName('sliders2');
+
+for (var i = 0; i < sliders2.length; i++) {
+
+    noUiSlider.create(sliders2[i], {
+        start: 0,
+        step: 1,
+        connect: "lower",
+        orientation: "horizontal",
+        range: {
+            'min': -100,
+            'max': 100
+        }
+    });
+    sliders2[i].noUiSlider.on('slide', setBC)
+}
+
+jQuery('#btn-bc').on('click', function () {
+    jQuery.ajax({
+        method: 'get',
+        url: '/mejora/mejora-brillo-contraste',
+        data: {
+            brillo: jQuery('#brightInp').val(),
+            contraste: jQuery('#contrastInp').val()
+        }
+    }).success(function (result) {
+        jQuery('.imagen').html('<img class="img img-responsive" src="data:images/jpeg;base64,'+ result + '" alt="">')
+    })
 });
-
-brillo.noUiSlider.on('end', function (value, handler) {
-    console.log(value)
-})
-
-var contraste = document.getElementById('contraste');
-noUiSlider.create(contraste, {
-    start: 0,
-    range: {'min': -100, 'max': 100},
-    step: 1
-});
-
-contraste.noUiSlider.on('end', function (value, handler) {
-    console.log(value)
-})
